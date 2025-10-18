28f5efcb53fd442cc6ec4080b94131f53cd2828b
Hiro <suhiro@gmail.com>
2025-10-18 11:32:47 -0400

    d

---
+++ public/build/assets/WebAuthLayout.vue_vue_type_script_setup_true_lang-C4OWx03z.js
@@ -0,0 +1,11 @@
+import{c as n}from"./createLucideIcon-DPfk0LRx.js";import{d as l,i as s,o,b as t,a as r,j as c,w as d,t as a,e as p,y as m,z as h}from"./app-Ca0afq3n.js";/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const B=n("LockIcon",[["rect",{width:"18",height:"11",x:"3",y:"11",rx:"2",ry:"2",key:"1w4ew1"}],["path",{d:"M7 11V7a5 5 0 0 1 10 0v4",key:"fwvmzm"}]]);/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const C=n("UserIcon",[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]]),f={class:"min-h-screen bg-[#1c1c1c] flex flex-col items-center justify-center px-4 py-8"},_={class:"w-full max-w-[400px]"},u={class:"text-center mb-8"},x={class:"flex items-center justify-center gap-3 mb-4"},y={class:"text-2xl font-bold text-[#ff6e02]"},k={key:0,class:"space-y-2"},v={key:0,class:"text-xl font-bold text-white"},w={key:1,class:"text-sm text-[#999999]"},b={class:"bg-[#374151] rounded-lg p-6 shadow-lg"},g={class:"mt-6 text-center text-xs text-[#999999]"},I=l({__name:"WebAuthLayout",props:{title:{},description:{}},setup(L){return(e,i)=>(o(),s("div",f,[t("div",_,[t("div",u,[r(p(m),{href:"/",class:"inline-block"},{default:d(()=>[t("div",x,[i[0]||(i[0]=t("div",{class:"text-4xl"},"👨‍🍳",-1)),t("span",y,a(e.$page.props.name),1)])]),_:1}),e.title||e.description?(o(),s("div",k,[e.title?(o(),s("h1",v,a(e.title),1)):c("",!0),e.description?(o(),s("p",w,a(e.description),1)):c("",!0)])):c("",!0)]),t("div",b,[h(e.$slots,"default")]),t("div",g,[t("p",null,"© 2024 "+a(e.$page.props.name)+". 版权所有",1)])])]))}});export{B as L,C as U,I as _};
---
+++ public/build/assets/useFormControl-Bxdfww90.js
@@ -0,0 +1,1 @@
+import{q as r,a as t}from"./useForwardExpose-DohNH0gZ.js";import{C as e}from"./app-Ca0afq3n.js";function a(o){return e(()=>r(o)?!!t(o)?.closest("form"):!0)}export{a as u};
